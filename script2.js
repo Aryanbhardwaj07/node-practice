8de61c5f15c1b35a68d0e374edcb23f53ce9ab3f
Aryanbhardwaj07 <firstnamearyanbhardwaj16@gmail.com>
2023-10-17 17:59:50 +0530

    Font art

--- script2.js
+++
@@ -1,8 +0,0 @@
-//install package
-npm install one-liner-joke --save
-//add this code
-console.log(getRandomJoke)
-// to run this code open terminal and write node script2 and press tab then enter
-code ./script2.js
-
-
